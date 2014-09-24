28eac6fe6e5d8346c5752cc83ec4edb78c9a7125
nir0s <nir36g@gmail.com>
2014-09-24 17:44:31 +0300

    linted, added todos

--- scripts/irc.js
+++ scripts/irc.js
@@ -1,9 +1,11 @@
+// TODO: MUCH better logging
+
 var irc = require('irc');
 var flowdock = require('flowdock');
-var async = require('async')
+var async = require('async');
 
 var fdFlowId = '080cded7-aeea-446f-a1bb-5f5e09e66f54';
-var fdApiKey = process.env.FLOWDOCK_API_KEY
+var fdApiKey = process.env.FLOWDOCK_API_KEY;
 var ircChannel = '#cloudify_test';
 var ircServer = 'irc.freenode.com';
 var relayUser = 'cosmo-admin';
@@ -14,59 +16,65 @@ var clients = {};
 var fds = new flowdock.Session(fdApiKey);
 
 
+// TODO: make this check for new users periodically
 async.waterfall(
     [
-        function(callback) {
-            fds.flows(function(flows) {
-                var flowIds;
-                flows.map(function(flow) {
-                    if (flow.id == fdFlowId) { callback(null, flow); }
+        function (callback) {
+            fds.flows(function (flows) {
+                flows.map(function (flow) {
+                    if (flow.id === fdFlowId) { callback(null, flow); }
                 });
             });
         },
 
-        function(flow, callback) {
-            flow.users.forEach(function(user) {
-                fdUsers[user.id] = user.nick
+        function (flow, callback) {
+            flow.users.forEach(function (user) {
+                fdUsers[user.id] = user.nick;
             });
-            callback(null, fdUsers)
+            callback(null, fdUsers);
         },
 
-        function(users, callback) {
-            for (var u in users) {
-                console.log('logging in: ' + users[u])
-                clients[u] = new irc.Client(ircServer, users[u], {
-                    channels: [ircChannel],
-                });
+        function (users) {
+            var u;
+            for (u in users) {
+                if (users.hasOwnProperty(u)) {
+                    console.log('logging in: ' + users[u]);
+                    clients[u] = new irc.Client(ircServer, users[u], {
+                        channels: [ircChannel],
+                    });
+                }
             }
         }
     ]
-)
+);
 
-relayClient = new irc.Client(ircServer, relayUser, {
+var relayClient = new irc.Client(ircServer, relayUser, {
     channels: [ircChannel],
 });
 
-relayClient.addListener('error', function(message) {
+relayClient.addListener('error', function (message) {
     console.log('error: ', message);
-    fds.message(fdFlowId, 'CHANNEL_ERROR: ' + JSON.stringify(message), ['irc_channel_error'])
+    fds.message(fdFlowId, 'CHANNEL_ERROR: ' + JSON.stringify(message), ['irc_channel_error']);
 });
 
 relayClient.addListener('message', function (from, to, message) {
     console.log('(' + to + ') ' + from + ': ' + message);
+    // TODO: only print message in flow if it doesn't contain the tag
     if (message.indexOf(ircChannel) < 0) {
         fds.message(fdFlowId, '(' + to + ') ' + from + ': ' + message, []);
     }
 });
 
-stream = fds.stream(fdFlowId);
-stream.on('message', function(message) {
+var stream = fds.stream(fdFlowId);
+stream.on('message', function (message) {
     console.log('flowdock: ' + JSON.stringify(message, undefined, 2));
-    if (message.event == 'message' && message.content.indexOf(ircChannel) < 0) {
-        if (message.user in clients) {
+    if (message.event === 'message' && message.content.indexOf(ircChannel) < 0) {
+        // TODO: verify non-registered user can send messages to flow
+        if (clients.hasOwnProperty(message.user)) {
+        // if (message.user in clients) {
             clients[message.user].say(ircChannel, message.content);
         } else {
-            relayClient.say(ircChannel, message.content)
+            relayClient.say(ircChannel, fdUsers[message.user] + ': ' + message.content);
         }
-    };
+    }
 });
